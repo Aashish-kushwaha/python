e1dfa85831e0382185f7e6f62e67d02e3e9c1f60
Aashish-kushwaha <kushwahaashu444@gmail.com>
2022-08-28 22:56:44 +0530

    compound interest

---
+++ compound_interest.py
@@ -0,0 +1,9 @@
+def compound_interest(p,r,t):
+    c=p*(pow((1+r/100),t))
+    return c-p
+
+p=int(input("enter the principal amount:"))
+r=float(input("enter the rate of interest:"))
+t=float(input("enter the time period:"))
+
+print("compound interest:",compound_interest(p,r,t))
