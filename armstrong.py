e61c865039fd6a5626e37ad3987c2947d2335dfe
Aashish-kushwaha <kushwahaashu444@gmail.com>
2022-08-29 00:07:57 +0530

    armstrong number

---
+++ armstrong.py
@@ -0,0 +1,17 @@
+def armstrong(n):
+    x=0
+    while n!=0:
+        r=n%10
+        x=r*r*r+x
+        n=n//10
+
+    
+    return x;
+
+n=int(input("enter number:"))
+r1=armstrong(n)
+if(n==r1):
+    print("number is armstrong")
+else:
+    print("number is not a armstrong")
+    
