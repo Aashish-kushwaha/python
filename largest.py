8983628abe2601625a519e1af7e3f9bf5ed8ea92
Aashish-kushwaha <kushwahaashu444@gmail.com>
2022-08-31 00:25:37 +0530

    lasrgest element in the array

---
+++ largest.py
@@ -0,0 +1,15 @@
+arr=[]
+n=int(input("enter number of input:"))
+for i in range(0,n):
+    no=int(input())
+    arr.append(no)
+
+print(arr)
+
+max=arr[0]
+for i in range(1,n):
+    if(arr[i]>max):
+        max=arr[i]
+
+print(max)
+
