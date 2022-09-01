f2f8a8f8c0285b181c3b300cb949a0910b68a650
Aashish-kushwaha <kushwahaashu444@gmail.com>
2022-09-01 22:20:21 +0530

    numpy creation

---
+++ creation_of_array.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+#using lsit for the creation of array
+arr=np.array([1,2,3])
+print("array of rank 1:\n",arr)
+#array of rank 2
+arr=np.array([[1,2,3],
+          [4,5,6]])
+print("array of rank 2:\n",arr)
+#array using tuple
+arr=np.array((1,2,3))
+print("array using tuple:\n",arr)
