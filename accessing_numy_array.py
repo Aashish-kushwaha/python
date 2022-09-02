92bde6ecad167dd56566aad477fae9728e0d1580
Aashish-kushwaha <kushwahaashu444@gmail.com>
2022-09-03 00:01:10 +0530

    mathematic and linear algebra on numpy array

--- accessing_numy_array.py
+++ accessing_numy_array.py
@@ -48,5 +48,49 @@ print()
 #the identity matrix
 a6=np.identity(4)
 print(a6)
+'''
+create the following matrix
+1 1 1 1 1 
+1 0 0 0 1
+1 0 9 0 1
+1 0 0 0 1
+1 1 1 1 1
+'''
+a7=np.ones((5,5))
+print(a7)
+a8=np.zeros((3,3))
+print(a8)
+a8[1,1]=9
+print(a8)
+a7[1:-1,1:-1]=a8
+print(a7)
+print()
+#coying array
+a9=np.array([1,2,3])
+b1=a9.copy()        #this way the new b1 matrix is created
+print(b1)
+
+#basic mathematics
+print("basic mathematics on an array")
+a10=np.array([1,2,3,4])
+print(a10)
+print("additon:",a10+2)
+print("subtraction:",a10-2)
+print("multiply:",a10*3)
+print("divide:",a10/2)
+print("power:",a10**2)
+
+b2=np.array([5,6,7,8])
+print("adding two matrix:",a10+b2)
+
+print("sin of elevry element:",np.sin(a10))
+print()
+print("cos of every element:",np.cos(a10))
 
 
+#linear algebra
+a11=np.ones((2,3))
+print(a11)
+b3=np.full((3,2),2)
+print(b3)
+print(np.matmul(a11,b3))
