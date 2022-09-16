faad36965a696f71d56859878142b79687dd9a26
Aashish-kushwaha <kushwahaashu444@gmail.com>
2022-09-17 00:32:09 +0530

    sets

--- SETS.py
+++ SETS.py
@@ -41,3 +41,32 @@ else:
 # Clear the sets
 mysets.clear()
 print(mysets)
+
+
+odds={1,3,5,7,9}
+evens={0,2,4,6,8}
+primes={2,3,5,7,11}
+
+#union
+u=odds.union(evens)
+print(u)
+
+#intersection
+i=odds.intersection(evens)
+print(i)
+
+#difference of two sets
+setA={1,2,3,4,5,6,7,8,9}
+setB={4,5,6,1,7,3}
+
+diff=setA.difference(setB)
+print(diff)
+#symetric difference
+d=setA.symmetric_difference((setB))
+print(d)
+
+#subset
+print(setB.issubset(setA))
+#superset
+print(setA.issuperset(setB))
+
