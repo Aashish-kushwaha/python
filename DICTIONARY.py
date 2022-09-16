09ef8f8b200d63c54a645fd9f115818eba70a8ad
Aashish-kushwaha <kushwahaashu444@gmail.com>
2022-09-16 23:03:12 +0530

    commit

---
+++ DICTIONARY.py
@@ -0,0 +1,7 @@
+# Dictionary: key-value pairs, Unorderd, mutable
+# Creating dictionary
+mydict={"name":"max","age":28,"city":"dehradun"}
+print(mydict)
+
+# using dict function
+mydict2=dict(name="mary",age=27,city="bostong")
